f7a7e6d8ad3fa8b50b5434ff38b37fd6b3037cd7
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-02 10:35:39 +0530

    [Pratibha Mastud] Add: Added:calculate length of line using cartecian system.

---
+++ Line_Comp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Line_Comp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+				int firstX, firstY, secondX, secondY;
+				double lengthOfLine;
+
+				Console.WriteLine("Enter the length of X1");
+
+				firstX = Convert.ToInt32(Console.ReadLine());
+
+				Console.WriteLine("Enter the length of Y1");
+
+				firstY = Convert.ToInt32(Console.ReadLine());
+
+				Console.WriteLine("Enter the length of Y2");
+
+				secondY = Convert.ToInt32(Console.ReadLine());
+
+				Console.WriteLine("Enter the length of X2");
+
+				secondX = Convert.ToInt32(Console.ReadLine());
+
+				lengthOfLine = Math.Sqrt((secondX - firstX) ^ 2 + (secondY - firstY) ^ 2);
+
+				Console.WriteLine("length of line is : " + lengthOfLine);
+			}
+
+		}
+	
+
+}	
