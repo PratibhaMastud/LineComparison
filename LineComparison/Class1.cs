b68a4ec147d759993db115a27f8549480f2aed1c
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-02 23:06:06 +0530

    [Pratibha Mastud] Add: Compare Two Line are Equal or Not

---
+++ LineComparison/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Class1
+{
+	public static void Main(String[] args)
+	{
+		Console.WriteLine("Welcome to Line Comparison Computation Program");
+		double length1;
+		double length2;
+		Random random = new Random();
+		double x1 = random.Next(10, 1);
+		double y1 = random.Next(10, 1);
+		double x2 = random.Next(10, 1);
+		double y1 = random.Next(10, 1);
+		double y2 = random.Next(10, 1);
+		double x3 = random.Next(10, 1);
+		double x4 = random.Next(10, 1);
+		double y3 = random.Next(10, 1);
+		double y4 = random.Next(10, 1);
+
+		length1 = (Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1))));
+		length2 = (Math.sqrt(((x4 - x3) * (x4 - x3)) + ((y4 - y3) * (y4 - y3))));
+
+		Double line1 = new Double(length1);
+		Double line2 = new Double(length2);
+		boolean equal = line1.Equals(line2);
+
+		if (equal)
+			Console.WriteLine("Lines are Equal");
+		else
+			Console.WriteLine("Lines are Not Equal");
+
+	}
+}
---
+++ LineComparison/LineComparison/LineComparisonProb.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace LineComparison
+{
+    class LineComparisonProb
+    {
+			Double[] lengths = new Double[2];
+			double firstLine, secondLine;
+			int firstX, firstY, secondX, secondY, thirdX, thirdY, fourthX, fourthY;
+
+			void calculateLengthOfLine()
+			{
+				Random random = new Random();
+				firstX = random.Next(10, 1);
+				firstY = random.Next(10, 1);
+				secondX = random.Next(10, 1);
+				secondY = random.Next(10, 1);
+				thirdX = random.Next(10, 1);
+				thirdY = random.Next(10, 1);
+				fourthX = random.Next(10, 1);
+				fourthY = random.Next(10, 1);
+
+				firstLine = Math.Sqrt(((secondX - firstX) * (secondX - firstX)) + ((secondY - firstY) * (secondY - firstY)));
+				secondLine = Math.Sqrt(((fourthX - thirdX) * (fourthX - thirdX)) + ((fourthY - thirdY) * (fourthY - thirdY)));
+
+				lengths[0] = Convert.ToDouble(firstLine);
+				lengths[1] = Convert.ToDouble(secondLine);
+
+				Double line1 = lengths[0];
+				Double line2 = lengths[1];
+				Console.WriteLine("Line1 length : {0}", lengths[0], " Line1 length : {0}", lengths[1]);
+
+				if (line1.Equals(line2) == true)
+				{
+					Console.WriteLine("_____________________________________________________________________________");
+					Console.WriteLine("Lines are equal to Length");
+				}
+				else
+					Console.WriteLine("_____________________________________________________________________________");
+				Console.WriteLine("Lines are not equal to Length");
+			}
+
+			public static void Main(String[] args)
+			{
+				LineComparisonProb lineComparison = new LineComparisonProb();
+				lineComparison.calculateLengthOfLine();
+			}
+		}
+	}
